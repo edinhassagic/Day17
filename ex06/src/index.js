70f503dbd4e4f40fd46a06df5af519ae74d6f3ea
edin hasagic <edinhassagic@gmail.com>
2021-03-24 23:40:25 +0100

    6

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,10 +1,10 @@
 const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function removeFirstTwo(list) {
-    "use strict";
+  "use strict";
 
-const [a, b, ...array2] = list;
+  const [a, b, ...array2] = list;
 
-    return array2;
+  return array2;
 }
 
 console.log(removeFirstTwo(array1));

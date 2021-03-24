0eb42adbe08e3b90c5d61e7d55348bb8b658c6f8
edin hasagic <edinhassagic@gmail.com>
2021-03-24 23:26:32 +0100

    2 fix

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,14 +1,12 @@
 const MY_CAR = {
-    wheels: 4,
-    cylinders: 4,
-    size: 2.2
+  wheels: 4,
+  cylinders: 4,
+  size: 2.2,
 };
 
 function main(myCar) {
-
-    
-    const {cylinders, size} = myCar;
-    return {cylinders, size};
+  const { cylinders, size } = myCar;
+  return { cylinders, size };
 }
 console.log(main(MY_CAR));
 module.exports = main;

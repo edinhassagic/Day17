77afe7f1949a72e16b714a4c456c778e2b79682f
edin hasagic <edinhassagic@gmail.com>
2021-03-24 23:20:46 +0100

    1

---
+++ ex08/src/index.js
@@ -0,0 +1,56 @@
+const _ = require('lodash');
+
+var users = [
+	{
+		firstName: "John",
+		lastName: "Doe",
+		age: 24,
+		gender: "male"
+	},
+	{
+		firstName: "Jane",
+		lastName: "Doe",
+		age: 5,
+		gender: "female"
+	},
+	{
+		firstName: "Jim",
+		lastName: "Carrey",
+		age: 54,
+		gender: "male"
+	},
+	{
+		firstName: "Kate",
+		lastName: "Winslet",
+		age: 40,
+		gender: "female"
+	}
+];
+
+function getUsers() {
+var output = "";
+for (let i = 0; i < users.length; i++) {
+    output += `${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender}\n`;
+    
+}
+return output;
+}
+
+function findUser(lastName, gender) {
+	try {
+		var user = _.find(users, (obj) => {
+			if (obj.lastName === lastName && obj.gender === gender) {
+				return true;
+			}
+		});
+		var iFindUser = `${user.firstName} ${user.lastName} is ${user.age}, ${user.gender}`;
+		return iFindUser;
+	} catch (error) {
+		return error;
+	}
+}
+
+getUsers();
+findUser("Doe", "male");
+console.log(findUser("Doe", "male"));
+module.exports = findUser;

e8e5f42425104815fc8ac5c20fc297a70944449f
edin hasagic <edinhassagic@gmail.com>
2021-03-25 02:03:54 +0100

    8

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,56 +1,29 @@
-const _ = require('lodash');
+const _ = require("lodash")
 
-var users = [
-	{
-		firstName: "John",
-		lastName: "Doe",
-		age: 24,
-		gender: "male"
-	},
-	{
-		firstName: "Jane",
-		lastName: "Doe",
-		age: 5,
-		gender: "female"
-	},
-	{
-		firstName: "Jim",
-		lastName: "Carrey",
-		age: 54,
-		gender: "male"
-	},
-	{
-		firstName: "Kate",
-		lastName: "Winslet",
-		age: 40,
-		gender: "female"
-	}
+let users = [
+  { firstName: "John", lastName: "Doe", age: 24, gender: "male" },
+  { firstName: "Jane", lastName: "Doe", age: 5, gender: "female" },
+  { firstName: "Jim", lastName: "Carrey", age: 54, gender: "male" },
+  { firstName: "Kate", lastName: "Winslet", age: 40, gender: "female" },
 ];
 
 function getUsers() {
-var output = "";
-for (let i = 0; i < users.length; i++) {
-    output += `${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender}\n`;
-    
-}
-return output;
+  var output = "";
+  for (var i = 0; i < users.length; i++) {
+    output += users[i].firstName + " " + users[i].lastName + " is " + users[i].age + ", " + users[i].gender + "\n\n";
+  }
+  return output;
 }
 
 function findUser(lastName, gender) {
-	try {
-		var user = _.find(users, (obj) => {
-			if (obj.lastName === lastName && obj.gender === gender) {
-				return true;
-			}
-		});
-		var iFindUser = `${user.firstName} ${user.lastName} is ${user.age}, ${user.gender}`;
-		return iFindUser;
-	} catch (error) {
-		return error;
-	}
+    try {
+     var user = _.find(users, {lastName, gender});
+     var iFindUser = user.firstName + " " + user.lastName + " is " + user.age + ", " + user.gender;
+     return iFindUser;
+    } catch (error) {
+      return "Cannot read property 'firstname' of undefined"; 
+    }
 }
-
-getUsers();
-findUser("Doe", "male");
-console.log(findUser("Doe", "male"));
-module.exports = findUser;
+  console.log(getUsers());
+  console.log(findUser("Doe", "male")); 
+  module.exports = findUser;
